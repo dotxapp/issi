d7d540996f8f8ebcaefda9d1f595c9544c8f840c
dotxapp <120318257+dotxapp@users.noreply.github.com>
2022-12-11 22:34:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-$path = "https://api.telegram.org/bot<yourtoken>;
+$message = $_GET['message'];
+$chatid= $_GET['chatid'];
+$token= $_GET['makin'];
 
-$update = json_decode(file_get_contents("php://input"), TRUE);
+$addr_details = unserialize(file_get_contents(
+'https://api.telegram.org/bot'.$token'/sendMessage?chat_id='.$$chatid'&text='.$message));
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
 
-if (strpos($message, "/weather") === 0) {
-$location = substr($message, 9);
-$weather = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=28dc55c477257ed3edc11b050708816f"), TRUE)["weather"][0]["main"];
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
-}
 ?>

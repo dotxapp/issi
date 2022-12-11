adc5775fcf6d027011d26fb44f5a7b95b981ed91
dotxapp <120318257+dotxapp@users.noreply.github.com>
2022-12-12 00:55:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,14 @@ $message = $_GET['message'];
 $chatid= $_GET['chatid'];
 $token= $_GET['makin'];
 
-$addr_details = unserialize(file_get_contents(
-'https://api.telegram.org/bot5683961460:AAGjgJj36-5rttL3c23iuWHtjgoGX9tjNyc/sendMessage?chat_id=-833002998&text=hello+world));
+$url = 'https://api.telegram.org/bot5683961460:AAGjgJj36-5rttL3c23iuWHtjgoGX9tjNyc/sendMessage?chat_id=-833002998&text=hello+world';
+  
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$response = json_decode($response);
+curl_close($ch);
+
 
 
 ?>

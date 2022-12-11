8204d601d5db742de4a6cabaa3a20d102442eb3d
dotxapp <120318257+dotxapp@users.noreply.github.com>
2022-12-11 23:19:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $chatid= $_GET['chatid'];
 $token= $_GET['makin'];
 
 $addr_details = unserialize(file_get_contents(
-'https://api.telegram.org/bot'.$token'/sendMessage?chat_id='.$$chatid'&text='.$message));
+'https://api.telegram.org/bot5683961460:AAGjgJj36-5rttL3c23iuWHtjgoGX9tjNyc/sendMessage?chat_id=-833002998&text=hello+world));
 
 
 ?>

